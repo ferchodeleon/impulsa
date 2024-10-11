302dae3618e7460e9c57b8e281e74d8a9a8f927f
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-11 17:23:47 -0500

    add component Info section with video and Who we are

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Banner } from "./components/Banner";
 import { Header } from "./components/Header";
+import { InfoSection } from "./components/InfoSection";
+import { WhoWeAre } from "./components/WhoWeAre";
 
 function App() {
   return (
     <main>
       <Header />
       <Banner />
+      <InfoSection />
+      <WhoWeAre />
     </main>
   );
 }
