1ba762ed8e8b9d28dba61765abfcde13cf3d8b85
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-15 15:16:50 -0500

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,51 @@
+import BirreteWhite from "../assets/images/icon-birrete-white.png";
+import LogoSuPresenciaCooperativa from "../assets/images/logo-cooperativa-white.png";
+
 import "../styles/Footer.css";
 
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <section>
+        <div className="footer-information">
+          <h2>Contáctanos por nuestros canales de atención:</h2>
+          <div>
+            <span className="footer-icon">
+              <img src={BirreteWhite} />
+            </span>
+            <p>
+              PBX: <a href="tel:+576017460101">[601] 7460101</a>
+            </p>
+          </div>
+          <div>
+            <span className="footer-icon">
+              <img src={BirreteWhite} />
+            </span>
+            <p>
+              WhatsApp:{" "}
+              <a
+                target="__blank"
+                href="https://api.whatsapp.com/send?phone=573208993195"
+              >
+                320 899 3195
+              </a>
+            </p>
+          </div>
+          <div>
+            <span className="footer-icon">
+              <img src={BirreteWhite} />
+            </span>
+            <p>servicioalasociado@supresenciacooperativa.com</p>
+          </div>
+        </div>
+        <div className="footer-logo">
+          <p>Organiza:</p>
+          <img
+            src={LogoSuPresenciaCooperativa}
+            alt="Logo de Su Presencia Cooperativa"
+          />
+        </div>
+      </section>
+    </footer>
+  );
 };
