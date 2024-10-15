77f6d2a7716a7d5859fe264979c6a20db681ea41
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-15 16:20:23 -0500

    Create component of speakers

---
+++ src/components/Speakers.jsx
@@ -0,0 +1,24 @@
+import "../styles/Speakers.css";
+
+export const Speakers = () => {
+  return (
+    <>
+      <section className="speakers-container">
+        <div>
+          <h2 className="speakers-title">Conoce nuestros conferencistas</h2>
+          <div className="speakers-image">
+            <div>
+              <img src="https://u2y8h9a9.rocketcdn.me/wp-content/uploads/2022/05/public-speaker-1.png" />
+            </div>
+            <div>
+              <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/34996/images/h9u2tGH1TKGUokFQX5uQ_obama-356133_1280.jpg" />
+            </div>
+            <div>
+              <img src="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
