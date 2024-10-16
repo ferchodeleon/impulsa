2ea0447eda694a461fbadde4ccb1e51039662386
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-16 10:09:35 -0500

    Create section of brands

---
+++ src/components/Brands.jsx
@@ -0,0 +1,43 @@
+import "../styles/Brands.css";
+
+export const Brands = () => {
+  return (
+    <section className="brands-container">
+      <div className="brands-section">
+        <h2>Marcas Participantes:</h2>
+        <div className="brands-images">
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+          <div className="brands-images-container">
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
