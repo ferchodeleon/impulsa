1d8ca37f3ea19e0a0d396f15b1892a2502ae505a
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 09:01:21 -0500

    Add images fake of speakers

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -8,13 +8,22 @@ export const Speakers = () => {
           <h2 className="speakers-title">Conoce nuestros conferencistas</h2>
           <div className="speakers-image">
             <div>
-              <img src="https://u2y8h9a9.rocketcdn.me/wp-content/uploads/2022/05/public-speaker-1.png" />
+              <div className="speakers-image-container">
+                <img src="https://u2y8h9a9.rocketcdn.me/wp-content/uploads/2022/05/public-speaker-1.png" />
+              </div>
+              <p className="speakers-name">Nombre de la persona</p>
             </div>
             <div>
-              <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/34996/images/h9u2tGH1TKGUokFQX5uQ_obama-356133_1280.jpg" />
+              <div className="speakers-image-container">
+                <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/34996/images/h9u2tGH1TKGUokFQX5uQ_obama-356133_1280.jpg" />
+              </div>
+              <p className="speakers-name">Nombre de la persona</p>
             </div>
             <div>
-              <img src="" />
+              <div className="speakers-image-container">
+                <img src="https://i.prcdn.co/img?regionKey=jDEdNOz6J6hT264T9DbOYQ%3D%3D" />
+              </div>
+              <p className="speakers-name">Nombre de la persona</p>
             </div>
           </div>
         </div>
