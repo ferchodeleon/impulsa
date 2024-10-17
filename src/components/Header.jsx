00faec328720d0c31c4b69a245f6f64b336a53da
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 11:47:40 -0500

    Changes for menu responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,39 @@
+import { useState } from "react";
+
 import "../styles/Header.css";
 
 import LogoImpulsa from "../assets/images/impulsa-logo.png";
 import Separator from "../assets/images/separator.png";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const onCloseMenu = () => setIsMenuOpen(false);
+  const isOpen = isMenuOpen ? "open" : "";
+
   return (
     <header>
       <div className="header-logo">
         <img src={LogoImpulsa} alt="Logo Impulsa" />
+        <button
+          className={`burger ${isOpen}`}
+          aria-label="Botón para abrir y cerrar el menú"
+          onClick={toggleMenu}
+        ></button>
       </div>
-      <div className="header-menu">
+      <div className={`header-background ${isOpen}`}></div>
+      <div className={`header-menu ${isOpen}`}>
         <nav>
           <ul>
-            <a>
-              <li>
+            <a style={{ animationDelay: `0.${2}s` }}>
+              <li onClick={onCloseMenu}>
                 <span className="header-menu__img">
                   <img src={Separator} alt="Icon Impulsa" />
                 </span>
                 ¿Quiénes somos?
               </li>
             </a>
-            <a>
+            <a style={{ animationDelay: "0.3s" }}>
               <li>
                 <span className="header-menu__img">
                   <img src={Separator} alt="Icon Impulsa" />
@@ -28,7 +41,7 @@ export const Header = () => {
                 Agenda
               </li>
             </a>
-            <a>
+            <a style={{ animationDelay: "0.4s" }}>
               <li>
                 <span className="header-menu__img">
                   <img src={Separator} alt="Icon Impulsa" />
@@ -36,7 +49,7 @@ export const Header = () => {
                 Conferencistas
               </li>
             </a>
-            <a>
+            <a style={{ animationDelay: "0.5s" }}>
               <li>
                 <span className="header-menu__img">
                   <img src={Separator} alt="Icon Impulsa" />
