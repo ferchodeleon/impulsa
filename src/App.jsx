7b911cadcba87b0d2638be10350a0c77811d7005
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 08:57:16 -0500

    Add component brands and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import { Banner } from "./components/Banner";
+import { Brands } from "./components/Brands";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { InfoSection } from "./components/InfoSection";
+import { Map } from "./components/Map";
 import { Speakers } from "./components/Speakers";
 import { WhoWeAre } from "./components/WhoWeAre";
 
@@ -14,6 +16,8 @@ function App() {
       <InfoSection />
       <WhoWeAre />
       <Speakers />
+      <Brands />
+      <Map />
       <Footer />
     </main>
   );
