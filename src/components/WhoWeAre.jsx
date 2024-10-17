15e8c82c72447d7f20c500e41c3f5738235b8916
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 09:07:28 -0500

    Section Who We are

---
+++ src/components/WhoWeAre.jsx
@@ -0,0 +1,20 @@
+import "../styles/WhoWeAre.css";
+
+export const WhoWeAre = () => {
+  return (
+    <section className="whoweare">
+      <div className="whoweare-container">
+        <h2 className="whoweare-title">¿Quienes somos?</h2>
+        <p className="whoweare-description">
+          Nuestro objetivo es fomentar el espíritu emprendedor y apoyar el
+          desarrollo de nuevas empresas a través de espacios que promuevan la
+          innovación, el networking y el intercambio de ideas entre
+          emprendedores, inversionistas y consumidores. A través de este
+          enfoque, buscamos desarrollar la capacidad comercial de los
+          participantes, impulsando la adquisición de nuevos clientes y la
+          expansión de sus empresas.
+        </p>
+      </div>
+    </section>
+  );
+};
