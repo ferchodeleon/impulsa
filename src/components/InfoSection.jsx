4fc0870cd544e209afec345612702e98ac8801fc
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 09:00:52 -0500

    Component Information of feria

---
+++ src/components/InfoSection.jsx
@@ -0,0 +1,70 @@
+import Exhibition from "../assets/images/exhibition.png";
+import Stand from "../assets/images/stand.png";
+import Attendants from "../assets/images/attendants.png";
+import Categories from "../assets/images/categories.png";
+import Speakers from "../assets/images/speakers.png";
+import DecorationInfo from "../assets/images/decoration-info.png";
+import IconBittereRed from "../assets/images/icon-birrete-red.png";
+import VideoImpulsa from "../assets/FERIA IMPULSA-VF.mp4";
+import "../styles/InfoSection.css";
+
+export const InfoSection = () => {
+  return (
+    <>
+      <section className="info-container">
+        <img
+          className="decoratino-info"
+          src={DecorationInfo}
+          alt="Imagen de varios Birretes"
+        />
+        <div className="info-section">
+          <div className="info-exhibition">
+            <img src={Exhibition} />
+            <p className="info-quantity">2</p>
+            <p className="info-description">Días de exhibición</p>
+          </div>
+          <div className="info-exhibition">
+            <img src={Stand} />
+            <p className="info-quantity">70</p>
+            <p className="info-description">Stands disponibles</p>
+          </div>
+          <div className="info-exhibition">
+            <img src={Attendants} />
+            <p className="info-quantity">+1000</p>
+            <p className="info-description">Asistentes diarios</p>
+          </div>
+          <div className="info-exhibition">
+            <img src={Categories} />
+            <p className="info-quantity">10</p>
+            <p className="info-description">Categorias comerciales</p>
+          </div>
+          <div className="info-exhibition">
+            <img src={Speakers} />
+            <p className="info-quantity">3</p>
+            <p className="info-description">Conferencistas</p>
+          </div>
+        </div>
+      </section>
+      <section className="info-video">
+        <div className="info-video-container">
+          <img
+            className="icon-left"
+            src={IconBittereRed}
+            alt="Icon Birrete en color rojo"
+          />
+          <img
+            className="icon-right"
+            src={IconBittereRed}
+            alt="Icon Birrete en color rojo"
+          />
+          <video
+            src={VideoImpulsa}
+            controls
+            preload="auto"
+            controlsList="nodownload"
+          />
+        </div>
+      </section>
+    </>
+  );
+};
