e6119c8bfa1b8b55e5b188feaf2fa4fdc7381477
Fernando De Leon Gil <gasnare@hotmail.com>
2024-10-17 09:00:07 -0500

    Component brands

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,4 +1,28 @@
 import "../styles/Brands.css";
+import Heloim from "../assets/brands/Heloim.jpg";
+import Bywc from "../assets/brands/ByW.jpg";
+import Aroma from "../assets/brands/aroma.jpg";
+import Asosalud from "../assets/brands/Asosalud.jpg";
+import Boxdes from "../assets/brands/Boxdes.jpg";
+import Brave from "../assets/brands/Brave.jpg";
+import Ceahi from "../assets/brands/Ceahi.jpg";
+import CrushCookies from "../assets/brands/CrushCookies.jpg";
+import Fileo from "../assets/brands/Fileo.jpg";
+import Forgive from "../assets/brands/Forgive.jpg";
+import LifeOn from "../assets/brands/Life-on.jpg";
+import Lizzeto from "../assets/brands/Lizzeto.jpg";
+import Lykke from "../assets/brands/Lykke.jpg";
+import ImageMR from "../assets/brands/MR.jpg";
+import Ojala from "../assets/brands/Ojala.jpg";
+import ParejaFinanzas from "../assets/brands/ParejaFinanzas.jpg";
+import Perfect from "../assets/brands/Perfect.jpg";
+import Pomposa from "../assets/brands/Pomposa.jpg";
+import Postreros from "../assets/brands/Postreros.jpg";
+import Preciada from "../assets/brands/Preciada.jpg";
+import Sonia from "../assets/brands/Sonia.jpg";
+import Thermo from "../assets/brands/Thermo.jpg";
+import Wale from "../assets/brands/Wale.jpg";
+import Hesed from "../assets/brands/Hesed.jpg";
 
 export const Brands = () => {
   return (
@@ -7,34 +31,76 @@ export const Brands = () => {
         <h2>Marcas Participantes:</h2>
         <div className="brands-images">
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Heloim} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Bywc} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Aroma} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Asosalud} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Boxdes} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Brave} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Ceahi} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={CrushCookies} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Fileo} />
           </div>
           <div className="brands-images-container">
-            <img src="" />
+            <img src={Forgive} />
+          </div>
+          <div className="brands-images-container">
+            <img src={LifeOn} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Lizzeto} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Lykke} />
+          </div>
+          <div className="brands-images-container">
+            <img src={ImageMR} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Ojala} />
+          </div>
+          <div className="brands-images-container">
+            <img src={ParejaFinanzas} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Perfect} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Pomposa} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Postreros} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Preciada} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Sonia} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Thermo} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Wale} />
+          </div>
+          <div className="brands-images-container">
+            <img src={Hesed} />
           </div>
         </div>
       </div>
